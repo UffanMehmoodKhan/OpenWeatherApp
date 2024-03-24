6300c088ec36e49d6f0fdf28f8d3b8091d0b5554
danish-61 <mrdanish2000@icloud.com>
2024-03-24 19:00:41 +0500

    databse testing

---
+++ src/database/testingDatabase.java
@@ -0,0 +1,27 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class testingDatabase
+{
+    public static void main(String[] args)
+    {
+        Connection conn = null;
+        try
+        {
+            // db parameters
+            String connectionString = "jdbc:sqlserver://DESKTOP-2DS0DMR;databaseName=testing;integratedSecurity=true;TrustServerCertificate=True";
+            // create a connection to the database//DESKTOP-2DS0DMR
+            conn = DriverManager.getConnection(connectionString);
+            System.out.println("Connection has been established.");
+        }
+        catch (SQLException e) 
+        {
+            System.out.println("connection failed");
+            System.out.println(e.getMessage());
+        }
+       
+    }
+}
