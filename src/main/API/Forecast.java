f2eca79ebccaf4ca2a572e7b90e6b2209f18065e
Huzyefah <huzyefahsaqib13@gmail.com>
2024-03-27 23:08:10 +0500

    Updated Error Handling

--- src/main/API/Forecast.java
+++ src/main/API/Forecast.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.net.*;
-import java.text.SimpleDateFormat;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import java.util.Date;
 import java.util.HashSet;
 
 public class Forecast {
+
+    @Deprecated
     // Method to get 5 day forecast data
-	@SuppressWarnings("deprecation")
     public String[] get5DayForecastData(String queryParameter, String apiKey) {
         List<String> forecastInfoList = new ArrayList<>();
         HashSet<String> uniqueDates = new HashSet<>();
+
         try {
             // Construct the URL for API request
             URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?" + queryParameter + "&appid=" + apiKey);
@@ -27,7 +27,16 @@ public class Forecast {
             // Check the HTTP response code
             int responseCode = con.getResponseCode();
             if (responseCode != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + responseCode);
+                System.out.println("Failed : HTTP error code : " + responseCode);
+                System.out.println("11111111111111111");
+                for (int i = 0; i < 4; i++) {
+                    forecastInfoList.add(null);
+                }
+                forecastInfoList.add("Not Found");
+                for (int i = 0; i < 36; i++) {
+                    forecastInfoList.add(null);
+                }
+                return forecastInfoList.toArray(new String[0]);
             } else {
                 // Read the API response
                 StringBuilder informationString = new StringBuilder();
@@ -42,27 +51,35 @@ public class Forecast {
                 JSONParser parser = new JSONParser();
                 JSONObject jsonObject = (JSONObject) parser.parse(informationString.toString());
 
-				
-				String countryName = (String) jsonObject.get("country");
-				JSONObject cityObject = (JSONObject) jsonObject.get("city");
-				String cityName = (String) cityObject.get("name");
-				JSONObject coordObject = (JSONObject) cityObject.get("coord");
-				Double latitude = (Double) coordObject.get("lat");
-				Double longitude = (Double) coordObject.get("lon");
-				forecastInfoList.add(cityName);
-				forecastInfoList.add(countryName);
-				forecastInfoList.add(latitude.toString());
-				forecastInfoList.add(longitude.toString());
-				// SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-				// Date date = new Date();  
-				// forecastInfoList.add(formatter.format(date).toString());
+                if (jsonObject.containsKey("error")) {
+                    System.out.println("Error: " + jsonObject.get("message"));
+                    System.out.println("222222222222222");
+                    for (int i = 0; i < 4; i++) {
+                        forecastInfoList.add(null);
+                    }
+                    forecastInfoList.add("Not Found");
+                    for (int i = 0; i < 36; i++) {
+                        forecastInfoList.add(null);
+                    }
+                    return forecastInfoList.toArray(new String[0]);
+                }
+
+                String countryName = (String) jsonObject.get("country");
+                JSONObject cityObject = (JSONObject) jsonObject.get("city");
+                String cityName = (String) cityObject.get("name");
+                JSONObject coordObject = (JSONObject) cityObject.get("coord");
+                Double latitude = (Double) coordObject.get("lat");
+                Double longitude = (Double) coordObject.get("lon");
+                forecastInfoList.add(cityName);
+                forecastInfoList.add(countryName);
+                forecastInfoList.add(latitude.toString());
+                forecastInfoList.add(longitude.toString());
 
                 // Extract relevant data from JSON object
                 JSONArray forecastList = (JSONArray) jsonObject.get("list");
-                
+
                 for (int i = 0; i < forecastList.size(); i++) {
-					
-					JSONObject forecastObject = (JSONObject) forecastList.get(i);
+                    JSONObject forecastObject = (JSONObject) forecastList.get(i);
                     String dateTime = (String) forecastObject.get("dt_txt");
                     String datee = dateTime.split(" ")[0];
 
@@ -70,7 +87,7 @@ public class Forecast {
                     if (uniqueDates.contains(datee)) {
                         continue;
                     }
-                    
+
                     uniqueDates.add(datee);
 
                     JSONObject mainObject = (JSONObject) forecastObject.get("main");
@@ -81,10 +98,9 @@ public class Forecast {
                     String weather = (String) weatherObject.get("main");
                     String weatherDescription = (String) weatherObject.get("description");
 
-					
                     // Convert temperature from Kelvin to Celsius
-                	Double minTempCelsius = minTemp.doubleValue() - 273.15;
-                	Double maxTempCelsius = maxTemp.doubleValue() - 273.15;
+                    Double minTempCelsius = minTemp.doubleValue() - 273.15;
+                    Double maxTempCelsius = maxTemp.doubleValue() - 273.15;
 
                     String formattedMinTemp = String.format("%.2f", minTempCelsius);
                     String formattedMaxTemp = String.format("%.2f", maxTempCelsius);
@@ -96,7 +112,6 @@ public class Forecast {
                     forecastInfoList.add(weather);
                     forecastInfoList.add(weatherDescription);
                 }
-
             }
 
         } catch (Exception e) {
@@ -104,5 +119,4 @@ public class Forecast {
         }
         return forecastInfoList.toArray(new String[0]);
     }
- 
 }
